617d32aa0ebb3313655b219799b0a44dfe0ec903
ikasiromii28 <camelliakamesan716@gmail.com>
2021-12-16 17:50:48 +0900

    指摘箇所の修正４

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index,:show]
   before_action :set_prototype, only: [:edit, :show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index,:show]
+  #before_action :move_to_index, except: [:index, :show]
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -21,13 +21,14 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
   end
 
   def update
@@ -51,11 +52,6 @@ class PrototypesController < ApplicationController
   end
 
   private
-  def move_to_index
-    unless user_signed_in? && current_user.id == @prototype.user_id
-      redirect_to action: :index
-    end
-  end
 
   def set_prototype
     @prototype = Prototype.find(params[:id])
@@ -64,5 +60,11 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
+
+  #def move_to_index
+   # unless user_signed_in? && current_user.id == @prototype.user_id
+    #  redirect_to action: :index
+    #end
+  #end
 
 end
